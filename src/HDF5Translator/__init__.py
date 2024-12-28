76bd68b52b2ee0561b314f1523e941231426ca35
toqduj <brian.pauw@bam.de>
2024-12-28 15:36:35 +0100

    Let's see if this helps...

---
+++ src/HDF5Translator/__init__.py
@@ -0,0 +1,3 @@
+from . import utils
+# from . import translator_elements
+# from . import translator
---
+++ src/HDF5Translator/utils/__init__.py
@@ -0,0 +1,5 @@
+from .validators import *
+from .argparse_utils import KeyValueAction
+from .configure_logging import configure_logging
+from .data_utils import *
+from .hdf5_utils import *
